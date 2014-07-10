fdb18de6e6320f270e8e8579e433c9c39f534f1f
Fabian Kirstein <fabi.kirstein@gmail.com>
2014-07-10 19:59:19 +0200

    Added Local Settings File

---
+++ config/settings_local.sample.py
@@ -0,0 +1,13 @@
+__author__ = 'fki'
+
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+    }
+}
