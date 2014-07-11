e5c56316d1804ecb8959d2413fc17a27b7c89257
almey <alanmeyerclp@gmail.com>
2014-07-12 01:06:30 +0200

    added eventsmanager to settings and urls

--- policycompass_services/urls.py
+++ policycompass_services/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pc_datamanager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^api/v1/', include('apps.eventsmanager.urls')),
 	url(r'^api/v1/', include('apps.metricsmanager.urls')),
     url(r'^api/v1/auth/', include('apps.common.urls')),
     url(r'^admin/', include(admin.site.urls)),
