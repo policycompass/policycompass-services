49b504dba89f7d28314fb87df371f58bd2c06c1f
Fabian Kirstein <fabi.kirstein@gmail.com>
2014-07-11 18:15:54 +0200

    Initial Commit

---
+++ apps/referencepool/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class PolicyDomain(models.Model):
+    pass
+
+
+class Language(models.Model):
+    pass
+
+
+class ExternalResource(models.Model):
+    pass
+
+
+class Unit(models.Model):
+    pass
