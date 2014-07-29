b71ad5a2352bb1cd5d555eea309e01c0cecef2b3
Fabian Kirstein <fabi.kirstein@gmail.com>
2014-07-29 11:19:07 +0200

    Changed to Django 1.7 migrations

--- apps/referencepool/migrations/0001_initial.py
+++ apps/referencepool/migrations/0001_initial.py
@@ -1,104 +1,84 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'PolicyDomain'
-        db.create_table('referencepool_policydomain', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal('referencepool', ['PolicyDomain'])
+    dependencies = [
+    ]
 
-        # Adding model 'Language'
-        db.create_table('referencepool_language', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('code', self.gf('django.db.models.fields.CharField')(unique=True, max_length=2)),
-            ('title', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
-        ))
-        db.send_create_signal('referencepool', ['Language'])
-
-        # Adding model 'ExternalResource'
-        db.create_table('referencepool_externalresource', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
-            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
-            ('api_url', self.gf('django.db.models.fields.URLField')(max_length=200)),
-        ))
-        db.send_create_signal('referencepool', ['ExternalResource'])
-
-        # Adding model 'UnitCategory'
-        db.create_table('referencepool_unitcategory', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
-        ))
-        db.send_create_signal('referencepool', ['UnitCategory'])
-
-        # Adding model 'Unit'
-        db.create_table('referencepool_unit', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
-            ('unit_category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['referencepool.UnitCategory'])),
-        ))
-        db.send_create_signal('referencepool', ['Unit'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'PolicyDomain'
-        db.delete_table('referencepool_policydomain')
-
-        # Deleting model 'Language'
-        db.delete_table('referencepool_language')
-
-        # Deleting model 'ExternalResource'
-        db.delete_table('referencepool_externalresource')
-
-        # Deleting model 'UnitCategory'
-        db.delete_table('referencepool_unitcategory')
-
-        # Deleting model 'Unit'
-        db.delete_table('referencepool_unit')
-
-
-    models = {
-        'referencepool.externalresource': {
-            'Meta': {'object_name': 'ExternalResource'},
-            'api_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
-        },
-        'referencepool.language': {
-            'Meta': {'object_name': 'Language'},
-            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '2'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
-        },
-        'referencepool.policydomain': {
-            'Meta': {'object_name': 'PolicyDomain'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
-        },
-        'referencepool.unit': {
-            'Meta': {'object_name': 'Unit'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
-            'unit_category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['referencepool.UnitCategory']"})
-        },
-        'referencepool.unitcategory': {
-            'Meta': {'object_name': 'UnitCategory'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['referencepool']
+    operations = [
+        migrations.CreateModel(
+            name='ExternalResource',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=100)),
+                ('url', models.URLField()),
+                ('api_url', models.URLField()),
+            ],
+            options={
+                'verbose_name': 'External Resource',
+                'verbose_name_plural': 'External Resources',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Language',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('code', models.CharField(unique=True, max_length=2)),
+                ('title', models.CharField(unique=True, max_length=100)),
+            ],
+            options={
+                'verbose_name': 'Language',
+                'verbose_name_plural': 'Languages',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='PolicyDomain',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=100)),
+                ('description', models.TextField()),
+            ],
+            options={
+                'verbose_name': 'Policy Domain',
+                'verbose_name_plural': 'Policy Domains',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Unit',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=50)),
+                ('description', models.TextField()),
+            ],
+            options={
+                'verbose_name': 'Unit',
+                'verbose_name_plural': 'Units',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UnitCategory',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=100)),
+            ],
+            options={
+                'verbose_name': 'Unit Category',
+                'verbose_name_plural': 'Unit Categories',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='unit',
+            name='unit_category',
+            field=models.ForeignKey(to='referencepool.UnitCategory'),
+            preserve_default=True,
+        ),
+    ]
