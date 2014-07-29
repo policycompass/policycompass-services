f06415e5b401ce978e56a25def4f1e6b7ad5c30c
Fabian Kirstein <fabi.kirstein@gmail.com>
2014-07-29 11:18:15 +0200

    Changed to Django 1.7 migrations

--- apps/metricsmanager/migrations/0001_initial.py
+++ apps/metricsmanager/migrations/0001_initial.py
@@ -1,191 +1,111 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'UnitCategory'
-        db.create_table('metricsmanager_unitcategory', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=100, unique=True)),
-        ))
-        db.send_create_signal('metricsmanager', ['UnitCategory'])
+    dependencies = [
+    ]
 
-        # Adding model 'Unit'
-        db.create_table('metricsmanager_unit', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=50, unique=True)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
-            ('unit_category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['metricsmanager.UnitCategory'])),
-        ))
-        db.send_create_signal('metricsmanager', ['Unit'])
-
-        # Adding model 'Metric'
-        db.create_table('metricsmanager_metric', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=100, unique=True)),
-            ('acronym', self.gf('django.db.models.fields.CharField')(max_length=20, unique=True)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
-            ('keywords', self.gf('django.db.models.fields.CharField')(max_length=400)),
-            ('geo_location', self.gf('django.db.models.fields.CharField')(blank=True, max_length=1000)),
-            ('publisher', self.gf('django.db.models.fields.CharField')(blank=True, max_length=200)),
-            ('details_url', self.gf('django.db.models.fields.URLField')(blank=True, max_length=500)),
-            ('license', self.gf('django.db.models.fields.CharField')(blank=True, max_length=100)),
-            ('user_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('language_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('ext_resource_id', self.gf('django.db.models.fields.IntegerField')(blank=True)),
-            ('created_at', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now_add=True)),
-            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('version', self.gf('django.db.models.fields.IntegerField')()),
-            ('formula', self.gf('django.db.models.fields.CharField')(max_length=10000, default='1')),
-            ('issued', self.gf('django.db.models.fields.DateField')()),
-            ('unit', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['metricsmanager.Unit'])),
-        ))
-        db.send_create_signal('metricsmanager', ['Metric'])
-
-        # Adding model 'MetricInDomain'
-        db.create_table('metricsmanager_metricindomain', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('domain_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('metric', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['metricsmanager.Metric'])),
-        ))
-        db.send_create_signal('metricsmanager', ['MetricInDomain'])
-
-        # Adding model 'RawData'
-        db.create_table('metricsmanager_rawdata', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('metric', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['metricsmanager.Metric'])),
-            ('row', self.gf('django.db.models.fields.PositiveIntegerField')()),
-            ('value', self.gf('django.db.models.fields.FloatField')()),
-            ('from_date', self.gf('django.db.models.fields.DateField')()),
-            ('to_date', self.gf('django.db.models.fields.DateField')()),
-        ))
-        db.send_create_signal('metricsmanager', ['RawData'])
-
-        # Adding model 'RawDataCategory'
-        db.create_table('metricsmanager_rawdatacategory', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=100, unique=True)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal('metricsmanager', ['RawDataCategory'])
-
-        # Adding model 'RawDataExtra'
-        db.create_table('metricsmanager_rawdataextra', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('metric', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['metricsmanager.Metric'])),
-            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['metricsmanager.RawDataCategory'])),
-        ))
-        db.send_create_signal('metricsmanager', ['RawDataExtra'])
-
-        # Adding model 'RawDataExtraData'
-        db.create_table('metricsmanager_rawdataextradata', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('raw_data_extra', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['metricsmanager.RawDataExtra'])),
-            ('row', self.gf('django.db.models.fields.PositiveIntegerField')()),
-            ('value', self.gf('django.db.models.fields.CharField')(max_length=200)),
-        ))
-        db.send_create_signal('metricsmanager', ['RawDataExtraData'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'UnitCategory'
-        db.delete_table('metricsmanager_unitcategory')
-
-        # Deleting model 'Unit'
-        db.delete_table('metricsmanager_unit')
-
-        # Deleting model 'Metric'
-        db.delete_table('metricsmanager_metric')
-
-        # Deleting model 'MetricInDomain'
-        db.delete_table('metricsmanager_metricindomain')
-
-        # Deleting model 'RawData'
-        db.delete_table('metricsmanager_rawdata')
-
-        # Deleting model 'RawDataCategory'
-        db.delete_table('metricsmanager_rawdatacategory')
-
-        # Deleting model 'RawDataExtra'
-        db.delete_table('metricsmanager_rawdataextra')
-
-        # Deleting model 'RawDataExtraData'
-        db.delete_table('metricsmanager_rawdataextradata')
-
-
-    models = {
-        'metricsmanager.metric': {
-            'Meta': {'object_name': 'Metric'},
-            'acronym': ('django.db.models.fields.CharField', [], {'max_length': '20', 'unique': 'True'}),
-            'created_at': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now_add': 'True'}),
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'details_url': ('django.db.models.fields.URLField', [], {'blank': 'True', 'max_length': '500'}),
-            'ext_resource_id': ('django.db.models.fields.IntegerField', [], {'blank': 'True'}),
-            'formula': ('django.db.models.fields.CharField', [], {'max_length': '10000', 'default': "'1'"}),
-            'geo_location': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '1000'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'issued': ('django.db.models.fields.DateField', [], {}),
-            'keywords': ('django.db.models.fields.CharField', [], {'max_length': '400'}),
-            'language_id': ('django.db.models.fields.IntegerField', [], {}),
-            'license': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'}),
-            'publisher': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '200'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'}),
-            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['metricsmanager.Unit']"}),
-            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'user_id': ('django.db.models.fields.IntegerField', [], {}),
-            'version': ('django.db.models.fields.IntegerField', [], {})
-        },
-        'metricsmanager.metricindomain': {
-            'Meta': {'object_name': 'MetricInDomain'},
-            'domain_id': ('django.db.models.fields.IntegerField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'metric': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['metricsmanager.Metric']"})
-        },
-        'metricsmanager.rawdata': {
-            'Meta': {'object_name': 'RawData', 'ordering': "['row']"},
-            'from_date': ('django.db.models.fields.DateField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'metric': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['metricsmanager.Metric']"}),
-            'row': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'to_date': ('django.db.models.fields.DateField', [], {}),
-            'value': ('django.db.models.fields.FloatField', [], {})
-        },
-        'metricsmanager.rawdatacategory': {
-            'Meta': {'object_name': 'RawDataCategory'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'})
-        },
-        'metricsmanager.rawdataextra': {
-            'Meta': {'object_name': 'RawDataExtra'},
-            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['metricsmanager.RawDataCategory']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'metric': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['metricsmanager.Metric']"})
-        },
-        'metricsmanager.rawdataextradata': {
-            'Meta': {'object_name': 'RawDataExtraData', 'ordering': "['row']"},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'raw_data_extra': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['metricsmanager.RawDataExtra']"}),
-            'row': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'value': ('django.db.models.fields.CharField', [], {'max_length': '200'})
-        },
-        'metricsmanager.unit': {
-            'Meta': {'object_name': 'Unit'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50', 'unique': 'True'}),
-            'unit_category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['metricsmanager.UnitCategory']"})
-        },
-        'metricsmanager.unitcategory': {
-            'Meta': {'object_name': 'UnitCategory'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'})
-        }
-    }
-
-    complete_apps = ['metricsmanager']
+    operations = [
+        migrations.CreateModel(
+            name='Metric',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=100)),
+                ('acronym', models.CharField(unique=True, max_length=20)),
+                ('description', models.TextField()),
+                ('keywords', models.CharField(max_length=400)),
+                ('geo_location', models.CharField(blank=True, max_length=1000)),
+                ('publisher', models.CharField(blank=True, max_length=200)),
+                ('details_url', models.URLField(blank=True, max_length=500)),
+                ('publisher_issued', models.DateField(null=True, blank=True)),
+                ('license', models.CharField(blank=True, max_length=100)),
+                ('unit_id', models.IntegerField()),
+                ('user_id', models.IntegerField()),
+                ('language_id', models.IntegerField()),
+                ('ext_resource_id', models.IntegerField(blank=True, default=0)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('version', models.IntegerField(editable=False)),
+                ('formula', models.CharField(max_length=10000, default='1')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='MetricInDomain',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('domain_id', models.IntegerField()),
+                ('metric', models.ForeignKey(to='metricsmanager.Metric')),
+            ],
+            options={
+                'verbose_name': 'Metric in Domain',
+                'verbose_name_plural': 'Metrics in Domains',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='RawData',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('row', models.PositiveIntegerField()),
+                ('value', models.FloatField()),
+                ('from_date', models.DateField()),
+                ('to_date', models.DateField()),
+                ('metric', models.ForeignKey(to='metricsmanager.Metric')),
+            ],
+            options={
+                'verbose_name': 'Raw Data',
+                'verbose_name_plural': 'Raw Data',
+                'ordering': ['row'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='RawDataCategory',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=100)),
+                ('description', models.TextField()),
+            ],
+            options={
+                'verbose_name': 'Raw Data Category',
+                'verbose_name_plural': 'Raw Data Categories',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='RawDataExtra',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('category', models.ForeignKey(to='metricsmanager.RawDataCategory')),
+                ('metric', models.ForeignKey(to='metricsmanager.Metric')),
+            ],
+            options={
+                'verbose_name': 'Raw Data Extra',
+                'verbose_name_plural': 'Raw Data Extras',
+                'ordering': ['id'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='RawDataExtraData',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('row', models.PositiveIntegerField()),
+                ('value', models.CharField(max_length=200)),
+                ('raw_data_extra', models.ForeignKey(to='metricsmanager.RawDataExtra')),
+            ],
+            options={
+                'verbose_name': 'Raw Data Extra Data',
+                'verbose_name_plural': 'Raw Data Extra Data',
+                'ordering': ['row'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
