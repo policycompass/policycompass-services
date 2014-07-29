45b6ae85b3f61ffb2bbcb40722081ce6e9a53321
Fabian Kirstein <fabi.kirstein@gmail.com>
2014-07-29 11:16:55 +0200

    Changed to Django 1.7 migrations

---
+++ apps/eventsmanager/migrations/0001_initial.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='HistoricalEvent',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('title', models.CharField(max_length=100)),
+                ('description', models.TextField(blank=True)),
+                ('keywords', models.CharField(blank=True, max_length=200)),
+                ('startEventDate', models.DateTimeField(blank=True)),
+                ('endEventDate', models.DateTimeField(blank=True)),
+                ('detailsURL', models.CharField(blank=True, max_length=200)),
+                ('geoLocation', models.CharField(blank=True, max_length=1000)),
+                ('relatedVisualisation', models.CharField(blank=True, max_length=200)),
+                ('languageID', models.IntegerField()),
+                ('userID', models.IntegerField()),
+                ('externalResourceID', models.IntegerField(blank=True, default=0)),
+                ('dateAddedToPC', models.DateTimeField(auto_now_add=True)),
+                ('dateIssuedByExternalResource', models.DateTimeField(auto_now_add=True)),
+                ('dateModified', models.DateTimeField(auto_now_add=True)),
+                ('viewsCount', models.IntegerField(blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
