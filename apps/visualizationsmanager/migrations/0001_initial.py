19d6b1a9b09968d0e9feb6d9f3c9b47f93891bde
miquel mila <miquel.mila@atos.net>
2014-09-15 16:11:30 +0200

    visualization model update

--- apps/visualizationsmanager/migrations/0001_initial.py
+++ apps/visualizationsmanager/migrations/0001_initial.py
@@ -13,31 +13,33 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='HistoricalEventsInVisualizations',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('visualization_id', models.IntegerField()),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('historical_event_id', models.IntegerField()),
                 ('description', models.TextField(blank=True)),
             ],
             options={
+                'verbose_name_plural': 'Historical Event in Visualization',
+                'verbose_name': 'Historical Event in Visualization',
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='MetricsInVisualizations',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('visualization_id', models.IntegerField()),
-                ('metric_id', models.IntegerField()),                
-                ('visualization_query', models.CharField(max_length=100)),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('metric_id', models.IntegerField()),
+                ('visualization_query', models.CharField(max_length=200)),
             ],
             options={
+                'verbose_name_plural': 'Metrics in Visualization',
+                'verbose_name': 'Metric in Visualization',
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Visualization',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('title', models.CharField(max_length=100)),
                 ('description', models.TextField(blank=True)),
                 ('keywords', models.CharField(blank=True, max_length=200)),
@@ -49,16 +51,28 @@ class Migration(migrations.Migration):
                 ('views_count', models.IntegerField()),
                 ('visualization_type_id', models.IntegerField()),
                 ('status_flag_id', models.IntegerField()),
-                ('filter_configuration',models.CharField(max_length=200)),
+                ('filter_configuration', models.CharField(max_length=200)),
             ],
             options={
             },
             bases=(models.Model,),
         ),
+        migrations.AddField(
+            model_name='metricsinvisualizations',
+            name='visualization',
+            field=models.ForeignKey(to='visualizationsmanager.Visualization'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='historicaleventsinvisualizations',
+            name='visualization',
+            field=models.ForeignKey(to='visualizationsmanager.Visualization'),
+            preserve_default=True,
+        ),
         migrations.CreateModel(
             name='VisualizationType',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('type', models.CharField(max_length=100)),
             ],
             options={
