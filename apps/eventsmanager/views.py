14573b07ec248019dd7cb8524d0e1588b13e9862
almey <alanmeyerclp@gmail.com>
2014-09-22 16:44:03 +0200

    added filter to events

--- apps/eventsmanager/views.py
+++ apps/eventsmanager/views.py
@@ -1,11 +1,33 @@
 from .models import Event
 from .serializers import EventSerializer
 from rest_framework import generics
+from datetime import datetime
+from django.db.models import Q
 
 
 class EventView(generics.ListCreateAPIView):
     model = Event
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        def validate_date(d):
+            try:
+                datetime.strptime(d, '%Y-%m-%d')
+                return True
+            except ValueError:
+                return False
+        queryset = Event.objects.all()
+        title = self.request.QUERY_PARAMS.get('title', None)
+        start = self.request.QUERY_PARAMS.get('start', None)
+        end = self.request.QUERY_PARAMS.get('end', None)
+        if start and end is not None:
+            if validate_date(start) and validate_date(end) is not None:
+                queryset2 = queryset
+                queryset = queryset.filter(Q(startEventDate__range=[start, end]) | Q(endEventDate__range=[start, end]))
+                queryset = queryset | queryset2.filter(startEventDate__lte=start).filter(endEventDate__gte=end)
+        if title is not None:
+            queryset = queryset.filter(title=title)
+        return queryset
 
 
 class EventInstanceView(generics.RetrieveUpdateDestroyAPIView):
