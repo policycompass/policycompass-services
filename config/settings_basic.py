c390d565d2e3f418029c8e7d476411999caec813
Nicolas Dietrich <nidi@mailbox.org>
2014-11-14 15:25:41 +0100

    Add X-User-Path and -Token to allowed CORS headers

--- config/settings_basic.py
+++ config/settings_basic.py
@@ -98,6 +98,17 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken',
+    'x-user-path',
+    'x-user-token',
+)
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
