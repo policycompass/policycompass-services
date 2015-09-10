6e731d24a5fa688745e4d4ffa71e4870fa71be30
Yves Müller <yves.mueller@liqd.de>
2015-09-10 17:25:34 +0200

    Define simple semantic actions
     * implement checking for pre defined names
     * add symbol table for variables

--- apps/metricsmanager/formula.py
+++ apps/metricsmanager/formula.py
@@ -15,3 +15,70 @@ def validate_formula(expr):
         get_parser().parse(expr)
     except FailedParse as e:
         raise ValidationError('Error parsing formular:\n%s' % str(e))
+
+
+class Sum:
+    def __init__(self, positive, negative):
+        self.positive = positive
+        self.negative = negative
+
+    def __repr__(self):
+        if len(self.negative) != 0:
+            s = " - %s " % " - ".join([ repr(x) for x in self.negative ])
+        else:
+            s = ""
+        return "%s%s" % (" + ".join([ repr(x) for x in self.positive ]), s)
+
+
+class Product:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __repr__(self):
+        if len(self.denominator) != 0:
+            s = " / %s " % " / ".join([ repr(x) for x in self.denominator ])
+        else:
+            s = ""
+        return "%s%s" % (" * ".join([ repr(x) for x in self.numerator ]), s)
+
+
+class Application:
+    def __init__(self, function_name, arguments):
+        self.function_name = function_name
+        self.arguments = arguments
+
+    def __repr__(self):
+        return "%s(%s)" % (self.function_name, ", ".join([ repr(x) for x in self.arguments]))
+
+class SemanticException(Exception):
+    pass
+
+class FormularSemantics():
+
+    def __init__(self, functions = [ "norm " ]):
+        self.functions = functions
+        self.indicator_variables = set()
+
+
+    def application(self, ast):
+        [ function_name, _ , args, last_arg, _ ] = ast
+        args.append(last_arg)
+
+        if function_name in self.functions:
+            return Application(function_name, args)
+        else:
+            raise SemanticException("Unkown function %s" % function_name)
+
+    def expression(self, ast):
+        return Sum(ast.get("positive"), ast.get("negative", []))
+
+    def variable(self, ast):
+        self.indicator_variables.add(ast)
+        return ast
+
+    def term(self, ast):
+        return Product(ast.get("numerator"), ast.get("denominator", []))
+
+    def constant(self, ast):
+        return float(ast)
--- apps/metricsmanager/tests/test_formula.py
+++ apps/metricsmanager/tests/test_formula.py
@@ -1,21 +1,23 @@
 from  django.test import TestCase
 
-from ..formula import get_parser
+from ..formula import get_parser, FormularSemantics
 from grako.exceptions import FailedParse
 
 class FormulaTest(TestCase):
 
-    def assert_parse(self, expr, expected_ast = None):
+    def assert_parse(self, expr, expected_ast = None, **kwargs):
 
         parser = get_parser()
-        ast = parser.parse(expr)
+        ast = parser.parse(expr, semantics = FormularSemantics(**kwargs))
+        print(ast)
 
         if expected_ast:
             self.assertEqual(ast, expected_ast)
 
     def test_basic_parser(self):
 
-        self.assert_parse("0.5 + 0.6")
-        self.assert_parse("0.5")
-        self.assert_parse("f(0.5 + 0.6)")
-        self.assert_parse("0.6 * nle(__1__) + 0.4 * ncm(__2__)")
+        # self.assert_parse("0.5 + 0.6")
+        # self.assert_parse("0.5")
+        # self.assert_parse("f(0.5 + 0.6)")
+        self.assert_parse("0.5 + 0.6 - 0.7")
+        self.assert_parse("0.6 * nle(__1__) + 0.4 * ncm(__2__)", functions = [ "nle", "ncm" ])
