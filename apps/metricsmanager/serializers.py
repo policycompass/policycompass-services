2db70beb74cc6820d9fdacffd10b29f044fb0b76
Yves Müller <yves.mueller@liqd.de>
2015-09-10 15:45:04 +0200

    Add formula validator

--- apps/metricsmanager/serializers.py
+++ apps/metricsmanager/serializers.py
@@ -1,7 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from .models import *
+from .formula import validate_formula
 
 
 class MetricSerializer(ModelSerializer):
+
+    formula = CharField(validators=[ validate_formula ])
+
     class Meta:
         model = Metric
