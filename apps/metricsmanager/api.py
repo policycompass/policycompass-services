09875d5b7135ac40075d6bebb362782b4d501470
Yves Müller <yves.mueller@liqd.de>
2015-09-22 14:49:06 +0200

    Add stub for operationalize endpoint

--- apps/metricsmanager/api.py
+++ apps/metricsmanager/api.py
@@ -5,7 +5,7 @@ from rest_framework import generics, status
 from django.core.exceptions import ValidationError
 from .models import *
 from .serializers import *
-from .formula import validate_formula
+from .formula import validate_formula, compute_formula
 
 class MetricsBase(APIView):
 
@@ -22,7 +22,6 @@ class MetricsBase(APIView):
         return Response(result)
 
 class FormulaValidate(APIView):
-
     def get(self, request):
         if "formula" not in request.QUERY_PARAMS:
             return Response({ "formula": "Can not be empty"}, status=status.HTTP_400_BAD_REQUEST)
@@ -39,3 +38,44 @@ class MetricsCreate(generics.CreateAPIView):
 class MetricsDetail(generics.RetrieveAPIView):
     model = Metric
     serializer_class = MetricSerializer
+
+
+"""
+
+Example data:
+
+{
+  "datasets": [
+    { "variable": "__1__", "dataset": 1 }
+  ]
+}
+
+
+"""
+class MetriscOperationalize(APIView):
+    def post(self, request, pk):
+        # check if metric exists
+        metric = Metric.objects.get(pk=pk)
+        if Metric.objects.get(pk=pk) is None:
+            return Response("Unable to find metric %s." % pk, status=status.HTTP_404_NOT_FOUND)
+
+        # check resquest data
+        serializer = OperationalizeSerializer(data=request.DATA, files=request.FILES)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # load required data sets
+
+        # compute result
+        mapping = serializer.object
+        formula = metric.formula
+        result = compute_formula(formula, mapping)
+
+        # store dataset
+        dataset_id = 1
+
+        return Response({
+            "dataset": {
+                "id": dataset_id
+            }
+        })
--- apps/metricsmanager/serializers.py
+++ apps/metricsmanager/serializers.py
@@ -1,11 +1,24 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework import serializers
 from .models import *
 from .formula import validate_formula
+from drf_compound_fields import fields as compound_fields
 
+class MetricSerializer(serializers.ModelSerializer):
 
-class MetricSerializer(ModelSerializer):
-
-    formula = CharField(validators=[ validate_formula ])
+    formula = serializers.CharField(validators=[ validate_formula ])
 
     class Meta:
         model = Metric
+
+class OperationalizeMappingSerializer(serializers.Serializer):
+    variable = serializers.RegexField("__[0-9]+__")
+    dataset = serializers.IntegerField(min_value=0)
+
+    def restore_object(self, validated_data, instance=None):
+        return (validated_data['variable'], validated_data['dataset'])
+
+class OperationalizeSerializer(serializers.Serializer):
+    datasets = compound_fields.ListField(OperationalizeMappingSerializer())
+
+    def restore_object(self, validated_data, instance=None):
+        return dict(validated_data['datasets'])
