09120b9478d54c611a9a13ad10bd3faa4a3ce152
almey <alanmeyerclp@gmail.com>
2015-10-07 15:28:59 +0200

    plugin changes

--- apps/eventsmanager/extractors/GESIS/__init__.py
+++ apps/eventsmanager/extractors/GESIS/__init__.py
@@ -8,8 +8,12 @@ def run(start, end, keyword):
 
     r = requests.get('http://www.vizgr.org/historical-events/search.php?begin_date=' + start + '&end_date=' + end + '&query=' + keyword + '&language=en&limit=10')
     #print(r.text)
-    result = xmltodict.parse(r.text)
+    try:
+        result = xmltodict.parse(r.text)
+    except:
+        result = False
     output = []
-    for key in result['result']['event']:
-        output.append({"title": key["description"], "description": key["description"], "date": key["date"].replace("/", "-")+"T00:00:00Z", "url": 'https://en.wikipedia.org/wiki/'+key["date"][:4]})
+    if result:
+        for key in result['result']['event']:
+            output.append({"title": key["description"], "description": key["description"], "date": key["date"].replace("/", "-")+"T00:00:00Z", "url": 'https://en.wikipedia.org/wiki/'+key["date"][:4]})
     return output
