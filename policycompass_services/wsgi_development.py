c001f3ff5e156afa95e3001a737ba4cc4c21cd62
Yves Müller <yves.mueller@liqd.de>
2015-10-14 13:41:08 +0200

    Add wsgi module for development setup

---
+++ policycompass_services/wsgi_development.py
@@ -0,0 +1,9 @@
+"""WSGI config for policycompass development setup
+
+It exposes the WSGI callable as a module-level variable named ``application`` running with
+development config.
+
+"""
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
