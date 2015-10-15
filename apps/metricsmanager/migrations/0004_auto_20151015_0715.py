e7ff341c2859366f4571750573d93a0398abd3cc
Yves Müller <yves.mueller@liqd.de>
2015-10-15 15:53:48 +0200

    Extend migration for json field in metrics
    - handle formerly incorrect json

--- apps/metricsmanager/migrations/0004_auto_20151015_0715.py
+++ apps/metricsmanager/migrations/0004_auto_20151015_0715.py
@@ -12,7 +12,17 @@ class Migration(migrations.Migration):
         ('metricsmanager', '0003_auto_20150930_1427'),
     ]
 
+    def create_valid_json(apps, schema_editor):
+        Metric = apps.get_model("metricsmanager", "Metric")
+        db_alias = schema_editor.connection.alias
+        for metric in Metric.objects.using(db_alias).all:
+            metric.variables = metric.variables.replace('\'','"')
+            metric.save()
+
     operations = [
+        migrations.RunPython(
+            create_valid_json,
+        ),
         migrations.AlterField(
             model_name='metric',
             name='variables',
