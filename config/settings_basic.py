9df294ce28733bbdd2684155c6403bb8f2f2ebf1
Yves Müller <yves.mueller@liqd.de>
2015-10-16 10:46:31 +0200

    Use single config variable for adhoracy location

--- config/settings_basic.py
+++ config/settings_basic.py
@@ -161,6 +161,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Path to adhocracy settings
-ADHOCRACY_BASE_URL = 'http://localhost:6541'
