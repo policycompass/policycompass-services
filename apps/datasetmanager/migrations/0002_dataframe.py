60b0024e7482909e687851a2cc12a01a1fdc84c6
Christos Botsikas <cbot@epu.ntua.gr>
2015-11-05 12:25:34 +0200

    dataset migration fails without database https://github.com/policycompass/policycompass/issues/364

--- apps/datasetmanager/migrations/0002_dataframe.py
+++ apps/datasetmanager/migrations/0002_dataframe.py
@@ -11,27 +11,28 @@ from ..dataset_data import *
 def rewrite_data_field(apps, schema_editor):
 
     datasets = Dataset.objects.all()
-    for d in datasets:
-        old_data = json.loads(d.data)
+    if datasets.exists():
+        for d in datasets.iterator():
+            old_data = json.loads(d.data)
 
-        if 'data_frame' in old_data:
-            print("Dataset " + str(d.pk) + " already updated. Nothing to do.")
-        else:
-            try:
-                data = DatasetDataTransformer.from_api(
-                    old_data,
-                    d.time_start,
-                    d.time_end,
-                    d.time_resolution,
-                    d.class_id,
-                    d.unit_id
-                )
-                d.data = data.get_json()
-                d.save()
-                print("Dataset " + str(d.pk) + " was updated.")
-            except Exception as e:
-                print("Could not update Dataset " + str(d.pk) + ". Will be deleted")
-                d.delete()
+            if 'data_frame' in old_data:
+                print("Dataset " + str(d.pk) + " already updated. Nothing to do.")
+            else:
+                try:
+                    data = DatasetDataTransformer.from_api(
+                        old_data,
+                        d.time_start,
+                        d.time_end,
+                        d.time_resolution,
+                        d.class_id,
+                        d.unit_id
+                    )
+                    d.data = data.get_json()
+                    d.save()
+                    print("Dataset " + str(d.pk) + " was updated.")
+                except Exception as e:
+                    print("Could not update Dataset " + str(d.pk) + ". Will be deleted")
+                    d.delete()
 
 
 class Migration(migrations.Migration):
