e2311c0e271175dea816754307f1cd44d73bdf6d
Cüneyt Hizli <cueneyt.hizli@fokus.fraunhofer.de>
2016-08-15 14:55:15 +0200

    renamed url for categories

--- apps/feedbackmanager/urls.py
+++ apps/feedbackmanager/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = patterns(
     '',
     url(r'^feedbacks$', FeedbackListView.as_view(), name='feedback-list'),
-    url(r'^feedbackcategories$', FeedbackCategoryListView.as_view(), name='feedbackcategory-list'),
+    url(r'^categories$', FeedbackCategoryListView.as_view(), name='feedbackcategory-list'),
     url(r'^$', Base.as_view(), name="feedbackmanager-base"),
 )
